1126c0349ee3a1a0db4f8bae135b99d6ce18f53a
sophiasussa <sophiasussa@outlook.com>
2024-03-11 15:40:33 -0300

    Nova classe model Edicao

---
+++ src/main/java/livraria/Model/Edicao.java
@@ -0,0 +1,27 @@
+package livraria.model;
+
+public class Edicao {
+    private int id;
+    private int ano;
+    private String novo_conteudo;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public String getNovo_conteudo() {
+        return novo_conteudo;
+    }
+    public void setNovo_conteudo(String novo_conteudo) {
+        this.novo_conteudo = novo_conteudo;
+    }
+    
+}
