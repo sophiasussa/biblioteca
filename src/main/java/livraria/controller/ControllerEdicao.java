6acb49e33b17176df755e776b3177c5c87b1563b
sophiasussa <sophiasussa@outlook.com>
2024-03-12 14:59:14 -0300

    ControllerEdicao

--- src/main/java/livraria/controller/ControllerEdicao.java
+++ src/main/java/livraria/controller/ControllerEdicao.java
@@ -1,8 +1,29 @@
 package livraria.controller;
 
+import java.util.List;
+
 import livraria.model.Edicao;
 import livraria.repository.DaoEdicao;
 
 public class ControllerEdicao {
-
+  	DaoEdicao dao = new DaoEdicao();
+	public boolean inserir(Edicao edicao) {
+		return dao.inserir(edicao);
+	}
+	
+	public boolean alterar(Edicao edicao) {
+		return dao.alterar(edicao);
+	}
+	
+	public boolean excluir(Edicao edicao) {
+		return dao.excluir(edicao);
+	}
+	
+	public List<Edicao> pesquisar(int id) {
+		return dao.pesquisar(id);
+	}
+	
+	public List<Edicao> pesquisarTodos() {
+		return dao.pesquisarTodos();
+	}  
 }
