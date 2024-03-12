be86b9acd5f8176347356501b2242ae70e2e493f
sophiasussa <sophiasussa@outlook.com>
2024-03-12 14:55:28 -0300

    ControllerEmprestimo

--- src/main/java/livraria/controller/ControllerEmprestimo.java
+++ src/main/java/livraria/controller/ControllerEmprestimo.java
@@ -1,8 +1,29 @@
 package livraria.controller;
 
+import java.util.List;
+
 import livraria.model.Emprestimo;
 import livraria.repository.DaoEmprestimo;
 
 public class ControllerEmprestimo {
-
+	DaoEmprestimo dao = new DaoEmprestimo();
+	public boolean inserir(Emprestimo emprestimo) {
+		return dao.inserir(emprestimo);
+	}
+	
+	public boolean alterar(Emprestimo emprestimo) {
+		return dao.alterar(emprestimo);
+	}
+	
+	public boolean excluir(Emprestimo emprestimo) {
+		return dao.excluir(emprestimo);
+	}
+	
+	public List<Emprestimo> pesquisar(int id) {
+		return dao.pesquisar(id);
+	}
+	
+	public List<Emprestimo> pesquisarTodos() {
+		return dao.pesquisarTodos();
+	}
 }
