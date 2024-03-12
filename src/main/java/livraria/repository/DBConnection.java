8f53153665b0b76a72b296e27b392cee57201fc3
sophiasussa <sophiasussa@outlook.com>
2024-03-12 14:31:24 -0300

    DBConnection

--- src/main/java/livraria/repository/DBConnection.java
+++ src/main/java/livraria/repository/DBConnection.java
@@ -1,5 +1,34 @@
 package livraria.repository;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+	private static DBConnection instance = null;
+	private Connection conn;
+	 
+	private String url = "jdbc:mysql://localhost:3306/livraria";
+	private String usuario = "root";
+	private String senha = "H123asd@";
+	 
+	private DBConnection() throws SQLException{
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(url, usuario, senha);
+		} catch (Exception ex) {
+	}}
+
+    public Connection getConnection() {
+        return conn;
+    }
+
+    public static DBConnection getInstance() throws SQLException{
+        if(instance == null) {
+            instance = new DBConnection();
+        }else if(instance.getConnection().isClosed()) {
+            instance = new DBConnection();
+        }
+        return instance;
+    }
 }
