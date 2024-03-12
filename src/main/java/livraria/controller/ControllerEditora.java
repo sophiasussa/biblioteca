eb9b4e335626b952773fe3beaa372f6b6576129c
sophiasussa <sophiasussa@outlook.com>
2024-03-12 14:57:38 -0300

    ControllerEditora

--- src/main/java/livraria/controller/ControllerEditora.java
+++ src/main/java/livraria/controller/ControllerEditora.java
@@ -1,8 +1,29 @@
 package livraria.controller;
 
+import java.util.List;
+
 import livraria.model.Editora;
 import livraria.repository.DaoEditora;
 
 public class ControllerEditora {
-
+  	DaoEditora dao = new DaoEditora();
+	public boolean inserir(Editora editora) {
+		return dao.inserir(editora);
+	}
+	
+	public boolean alterar(Editora editora) {
+		return dao.alterar(editora);
+	}
+	
+	public boolean excluir(Editora editora) {
+		return dao.excluir(editora);
+	}
+	
+	public List<Editora> pesquisar(int id) {
+		return dao.pesquisar(id);
+	}
+	
+	public List<Editora> pesquisarTodos() {
+		return dao.pesquisarTodos();
+	}  
 }
