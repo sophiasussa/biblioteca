9d3ba8b675d624f309468c976267dbce1615eba2
sophiasussa <sophiasussa@outlook.com>
2024-03-12 15:00:38 -0300

    ControllerAutor

--- src/main/java/livraria/controller/ControllerAutor.java
+++ src/main/java/livraria/controller/ControllerAutor.java
@@ -1,8 +1,29 @@
 package livraria.controller;
 
+import java.util.List;
+
 import livraria.model.Autor;
 import livraria.repository.DaoAutor;
 
 public class ControllerAutor {
-
+  	DaoAutor dao = new DaoAutor();
+	public boolean inserir(Autor autor) {
+		return dao.inserir(autor);
+	}
+	
+	public boolean alterar(Autor autor) {
+		return dao.alterar(autor);
+	}
+	
+	public boolean excluir(Autor autor) {
+		return dao.excluir(autor);
+	}
+	
+	public List<Autor> pesquisar(int id) {
+		return dao.pesquisar(id);
+	}
+	
+	public List<Autor> pesquisarTodos() {
+		return dao.pesquisarTodos();
+	}  
 }
